feaec1b08a63f6218738fe5d6c422ece89719787
natewu <36091727+natewu@users.noreply.github.com>
2021-01-04 16:02:50 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+  render(){
+    return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +21,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )  
+  }
 }
 
 export default App;
