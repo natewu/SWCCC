d9e1938435757c79d1692225619ef8e31f5a9be4
natewu <36091727+natewu@users.noreply.github.com>
2021-01-08 10:15:36 -0700

    Co-authored-by: Andrew Chu <yolo4zhou@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import { Component } from 'react'
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import titleBar from './components/titlebar';
 
 class App extends Component{
   render(){
     return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        hello
+        <titleBar/>
       </header>
     </div>
     )  
---
+++ src/components/titlebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class titlebar extends Component {
+    render() {
+        return (
+            <div>
+                hello test
+            </div>
+        )
+    }
+}
+
+export default titlebar
